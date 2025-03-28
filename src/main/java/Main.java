3953810fbb3716100fc46361762a8235c65caf18
Daniil Krivko <krivko@ut.ee>
2025-03-28 21:21:39 +0200

    refact

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,22 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.sql.*;
 
 public class Main {
     private static Connection connection;
     private static final String DB_PATH = "database.db";
 
-    public static void main(String[] args) throws SQLException, IOException {
+    public static void main(String[] args) throws SQLException, IOException, NoSuchAlgorithmException, InvalidKeySpecException {
         connection = initiateConnection(DB_PATH);
 
         try (ServerSocket ss = new ServerSocket(8080)) {
             while (true) {
                 try (Socket socket = ss.accept()) {
-                    User user = handleClient(socket);
-                    user.handleSession(socket);
-                    ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
-                    output.writeObject(new Packet(Queries.getAllOrders(connection))); // basic query for testing
+                    User user = handleClient(socket, connection);
+                    if (user != null) user.handleSession(socket);
                 }
             }
         }
@@ -26,27 +26,45 @@ public class Main {
         return DriverManager.getConnection("jdbc:sqlite:" + pathname);
     }
 
-    // TODO: handle client log in (provide username and password), then verify his role and create object
-    private static User handleClient(Socket socket) throws RuntimeException {
-        User user = null;
+    // TODO: handle client log in (provide username and password), then verify his role and create object. Move to Helpers!
+    private static User handleClient(Socket socket, Connection connection) throws RuntimeException, SQLException, NoSuchAlgorithmException, InvalidKeySpecException {
         try {
-            BufferedReader br = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-            PrintWriter wr = new PrintWriter(socket.getOutputStream(), true);
+            DataInputStream input = new DataInputStream(socket.getInputStream());
+            DataOutputStream output = new DataOutputStream(socket.getOutputStream());
 
-            wr.println("hello, must be working");
-            String username = br.readLine();
-            wr.println("no way it is working. give password");
-            String password = br.readLine();
-            System.out.println("wow: " + username + " " + password);
+            output.writeUTF("Enter username:");
+            String username = input.readUTF();
 
-            // verify id, password, role and assign role to user
+            output.writeUTF("Enter password:");
+            String password = input.readUTF();
+
+            // clumsy debuggins
+            System.out.println("Received login: " + username + " / " + password);
+
+            int userID = Queries.getUserID(connection, username);
+            if (userID == -1) {
+                System.out.println("user id is: " + userID);
+                output.writeUTF("Invalid"); // REPLACE WITH PROTOCOL CODES LATER
+                System.out.println("wrong login/server side");
+                return null;}
+
+            String role;
+
+            if (Helpers.verifyPassword(connection, userID, password)){
+                output.writeUTF("login good");
+                role = Queries.getUserRole(connection, userID);
+                return switch (role) {
+                    case "customer" -> new Customer(userID, connection);
+                    //case "admin" -> new Admin(userID, connection);  more roles later
+                    default -> null;
+                };
+            }
+            output.writeUTF("wrong password");
+            return null;
 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Error handling client: " + e.getMessage(), e);
         }
-
-        return user;
     }
-
 
 }
