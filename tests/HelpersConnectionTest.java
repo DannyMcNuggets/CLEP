03bce7d6dd484c5e7e53df3f9157c5f4dfdfab66
Daniil Krivko <krivko@ut.ee>
2025-04-15 11:32:23 +0300

    unused

--- tests/HelpersConnectionTest.java
+++ tests/HelpersConnectionTest.java
@@ -1,4 +1,5 @@
 import CLEP.util.Helpers;
+import CLEP.util.Queries;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -18,15 +19,19 @@ import static org.testng.AssertJUnit.*;
 
 public class HelpersConnectionTest {
 
+    /* UNUSED ANYMORE
     private static final String TEST_DB_PATH ="test_database.db";
     private static final File dbFile = new File(TEST_DB_PATH);
     private static final String scriptSQL = "dbscript.sql";
     private static Connection connection;
+    private static Queries queries;
 
     @BeforeClass
     static void setUp() throws SQLException, IOException {
 
-        if (dbFile.exists()) { dbFile.delete();}
+        if (dbFile.exists()) {
+            dbFile.delete();
+        }
 
         boolean created = dbFile.createNewFile();
         if (created){
@@ -39,59 +44,11 @@ public class HelpersConnectionTest {
         if (connection == null){
             System.out.println("damn");
         }
+
+        queries = new Queries(connection);
+
         executeScript(connection, scriptSQL);
     }
-
-    @Test
-    public void registerUserSuccess() throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException {
-        boolean registered = Helpers.registerUser(connection, "Username123", "ValidPass123", "validemail@mail.com", "customer");
-        assertTrue(registered);
-    }
-
-
-    @Test
-    public void registerUserNameTakenFail() throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException {
-        String registerName = "NewUser12";
-        String registerPassword = "ValidPass123";
-        String registerEmail = "validemail@mail.com";
-        String registerRole = "customer";
-        boolean registered1 = Helpers.registerUser(connection, registerName, registerPassword, registerEmail, registerRole);
-        boolean registered2 = Helpers.registerUser(connection, registerName, registerPassword, registerEmail, registerRole);
-        assertFalse(registered2);
-
-    }
-
-    @Test
-    //login(Connection connection, String name, String password)
-    public void loginCustomerSuccess() throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException {
-        String loginName = "LoginTester";
-        String loginPassword = "ValidPass123";
-        Helpers.registerUser(connection, loginName, loginPassword, "logintester@gmail.com", "customer");
-        String expectedRole = "customer";
-        String recievedRole = Helpers.login(connection, loginName, loginPassword);
-        assertEquals(expectedRole, recievedRole);
-    }
-
-    @Test
-    public void loginCustomerFail() throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException {
-        String loginName = "LoginTesterFailer";
-        String loginPassword = "ValidPass123";
-        String loginWrongPassword = "ValidPass321";
-        Helpers.registerUser(connection, loginName, loginPassword, "logintester@gmail.com", "customer");
-        String recievedRole = Helpers.login(connection, loginName, loginWrongPassword);
-        assertNull(recievedRole);
-    }
-
-
-
-
-
-
-
-
-
-
-
 
 
     @AfterClass
@@ -100,6 +57,7 @@ public class HelpersConnectionTest {
         connection.close();
         dbFile.delete();
     }
+
 
     private static void executeScript(Connection connection, String scriptPath) throws SQLException, IOException {
         String sqlScript = new String(Files.readAllBytes(Paths.get(scriptPath)));
@@ -118,5 +76,6 @@ public class HelpersConnectionTest {
         }
     }
 
+     */
 
 }
