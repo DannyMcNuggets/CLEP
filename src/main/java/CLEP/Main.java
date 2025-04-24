acdc0a564a32e1883c01975a117225660b220494
Daniil Krivko <krivko@ut.ee>
2025-04-24 19:48:37 +0300

    typo fix

--- src/main/java/CLEP/Main.java
+++ src/main/java/CLEP/Main.java
@@ -1,20 +1,9 @@
 package CLEP;
 
-import CLEP.UserRoles.User;
-
 import java.io.*;
 import java.net.ServerSocket;
-import java.net.Socket;
-import java.security.NoSuchAlgorithmException;
-import java.security.spec.InvalidKeySpecException;
 import java.sql.*;
-
-import CLEP.auth.Auth;
-import CLEP.auth.Register;
 import CLEP.util.ClientConnection;
-import CLEP.util.Helpers;
-import CLEP.util.IOUnit;
-import CLEP.util.Queries;
 
 public class Main {
     private static final String DB_PATH = "database.db";
